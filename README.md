b4c6f2e88ff058c63c3c32805d3d166d47f5c92b
trevinomatt <mattrtrevino@gmail.com>
2021-03-17 05:58:00 -0500

    clone mern-starter-kit

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # mern-tailwind
+
+To use this application,
+
+1. make development.js file inside config folder
+2. put mongoDB info into dev.js file
+3. Type  " npm install " inside the root directory  ( Download Server Dependencies )
+4. Type " npm install " inside the client directory ( Download Front-end Dependencies )
+5. Type " npm run dev " inside the server directory ( Start this application )
+
+### Project Stack
+<div><img src="https://img.shields.io/badge/Node.js-91C640?style=flat-square&logo=node.js&logoColor=white"/></div>
+<div><img src="https://img.shields.io/badge/Express-000000?style=flat-square&logo=Express&logoColor=white"/></div>
+<div><img src="https://img.shields.io/badge/React-61DAFB?style=flat-square&logo=React&logoColor=black"/></div>
+<div><img src="https://img.shields.io/badge/Redux-764ABC?style=flat-square&logo=Redux&logoColor=white"/></div>
+<div><img src="https://img.shields.io/badge/MongoDB-47A248?style=flat-square&logo=MongoDB&logoColor=white"/></div>
+<div><img src="https://img.shields.io/badge/JSON Web Tokens-000000?style=flat-square&logo=JSON-Web-Tokens&logoColor=white"/></div>
+<div><img src="https://img.shields.io/badge/Tailwind CSS-38B2AC?style=flat-square&logo=Tailwind-CSS&logoColor=white"/></div>
---
+++ server/models/User.js
@@ -0,0 +1,105 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        trim: true,
+        unique: 1
+    },
+    password: {
+        type: String,
+        minlength: 5
+    },
+    lastname: {
+        type: String,
+        maxlength: 50
+    },
+    role: {
+        type: Number,
+        default: 0
+    },
+    image: String,
+    token: {
+        type: String
+    },
+    tokenExp: {
+        type: Number
+    }
+})
+
+userSchema.pre('save', function( next ){
+    let user = this;
+
+    if (user.isModified('password')) {
+        console.log('user isModified');
+        // password bcrypt
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if (err) {
+                return next(err);
+            } else {
+                bcrypt.hash(user.password, salt, function(err, hash) {
+                    if (err) {
+                        return next(err);
+                    } else {
+                        user.password = hash;
+                        next();
+                    }
+                })
+            }
+        });
+    } else {
+        next();
+    }
+
+})
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+        if (err) {
+            return cb(err);
+        } else {
+            return cb(null, isMatch);
+        }
+    })
+}
+
+userSchema.methods.generateToken = function(cb) {
+    let user = this;
+    // generate token with jsonwebtoken
+    let token = jwt.sign(user._id.toHexString(), 'tokenMatthew');
+
+    user.token = token;
+    user.save(function(err, user) {
+        if (err) {
+            return cb(err);
+        } else {
+            cb(null, user);
+        }
+    })
+}
+
+userSchema.statics.findByToken = function(token, cb) {
+    let user = this;
+
+    // token decode
+    jwt.verify(token, 'tokenMatthew', function(err, decoded) {
+        user.findOne({"_id": decoded, "token": token}, function(err, user) {
+            if (err) {
+                return cb(err);
+            } else {
+                return cb(null, user);
+            }
+        })
+    })
+}
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = { User }
