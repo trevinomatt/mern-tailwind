98c9da1f0a765c8b42cfbdeade37eaefc4ac9b15
trevinomatt <mattrtrevino@gmail.com>
2021-03-19 19:45:03 -0500

    add cors & Routes

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,34 +1,12 @@
-import React, { useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 import { withRouter } from 'react-router-dom';
 
 function LandingPage(props) {
-
-    useEffect(() => {
-        axios.get('/api/hello')
-        .then(response => console.log(response.data))
-    }, [])
-
-    const onClickHandler = () => {
-        axios.get('/api/users/logout')
-            .then(response => {
-                if (response.data.success) {
-                    props.history.push('/login');
-                } else {
-                    alert('failed logout');
-                }
-            });
-    }
-
     return (
         <div style={{
             display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh'
         }}>
             <h2>LandingPage</h2>
-
-            <button onClick={onClickHandler}>
-                Logout
-            </button>
         </div>
     )
 }
--- client/src/hoc/auth.js
+++ client/src/hoc/auth.js
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { auth } from '../_actions/user_action';
 
 export default function (SpecificComponent, option, adminRoute = null) {
 
     function AuthenticationCheck(props) {
 
+        const user = useSelector(state => state.user);
         const dispatch = useDispatch();
 
         useEffect(() => {
@@ -31,7 +32,7 @@ export default function (SpecificComponent, option, adminRoute = null) {
         }, [])
 
         return (
-            <SpecificComponent />
+            <SpecificComponent {...props} user={user} />
         )
     }
 
